3970a39e2f64df35fbb309942f83e22f99fac31c
bugatuasistentevirtualaws <guacaricontigoaws@gmail.com>
2025-02-09 22:00:59 -0500

    update user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ export type TUser = {
   id: number;
   email: string;
   recoveryEmail: string;
+  mustUpdatePassword: boolean;
   status: string;
   createdAt: Date;
   updatedAt: Date;
@@ -13,6 +14,7 @@ function parseUser(json: any): TUser {
     email: json?.email ?? '',
     recoveryEmail: json?.recovery_email ?? '',
     status: json?.status ?? 'inactive',
+    mustUpdatePassword: json?.must_update_password ?? false,
     createdAt: json?.created_at ?? new Date(),
     updatedAt: json?.updated_at ?? new Date(),
   };
